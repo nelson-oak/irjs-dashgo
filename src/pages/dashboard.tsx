ac8a89e59c38c496c24052ae28eaa552d7a9363b
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-05-25 19:58:32 -0300

    Aula 'Dashboard responsivo' (C4-M1-SM4)

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -82,7 +82,7 @@ export default function Dashboard() {
           align="flex-start"
         >
           <Box
-            p="8"
+            p={['6', '8']}
             bg="gray.800"
             borderRadius={8}
             pb="4"
@@ -104,7 +104,7 @@ export default function Dashboard() {
 
 
           <Box
-            p="8"
+            p={['6', '8']}
             bg="gray.800"
             borderRadius={8}
             pb="4"
