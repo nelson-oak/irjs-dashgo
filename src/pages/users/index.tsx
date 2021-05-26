02d8f481aac7df14ccad33cf2b0556f2798d9e4c
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-05-26 20:05:31 -0300

    Aula 'Listagem de usuários' (C4-M2-SM1)

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -11,9 +11,24 @@ import { Sidebar } from "../../components/Sidebar";
 export default function UserList() {
   const { data, isLoading, error } = useQuery('users', async () => {
     const response = await fetch('http://localhost:3000/api/users');
-    const data = response.json()
+    const data = await response.json()
+
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        })
+      }
+    })
+
+
     
-    return data
+    return users
   })
 
   const isWideVersion = useBreakpointValue({
@@ -103,83 +118,33 @@ export default function UserList() {
                 </Thead>
 
                 <Tbody>
-                  <Tr>
-                    <Td
-                      px={['4', '4',]}
-                    >
-                      <Checkbox
-                        colorScheme="pink"
-                      />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text
-                          fontWeight="bold"
-                        >
-                          Nelson Carvalho
-                        </Text>
-                        <Text
-                          fontSize="small"
-                          color="gray.300"
-                        >
-                          nelson.oak.13@gmail.com
-                        </Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>13 de Março, 2021</Td>}
-                  </Tr>
-
-                  <Tr>
-                    <Td
-                      px={['4', '4',]}
-                    >
-                      <Checkbox
-                        colorScheme="pink"
-                      />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text
-                          fontWeight="bold"
-                        >
-                          Nelson Carvalho
-                        </Text>
-                        <Text
-                          fontSize="small"
-                          color="gray.300"
-                        >
-                          nelson.oak.13@gmail.com
-                        </Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>13 de Março, 2021</Td>}
-                  </Tr>
-
-                  <Tr>
-                    <Td
-                      px={['4', '4',]}
-                    >
-                      <Checkbox
-                        colorScheme="pink"
-                      />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text
-                          fontWeight="bold"
-                        >
-                          Nelson Carvalho
-                        </Text>
-                        <Text
-                          fontSize="small"
-                          color="gray.300"
-                        >
-                          nelson.oak.13@gmail.com
-                        </Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>13 de Março, 2021</Td>}
-                  </Tr>
+                  {data.map(user => (
+                    <Tr key={user.id}>
+                      <Td
+                        px={['4', '4',]}
+                      >
+                        <Checkbox
+                          colorScheme="pink"
+                        />
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Text
+                            fontWeight="bold"
+                          >
+                            {user.name}
+                          </Text>
+                          <Text
+                            fontSize="small"
+                            color="gray.300"
+                          >
+                            {user.email}
+                          </Text>
+                        </Box>
+                      </Td>
+                      {isWideVersion && <Td>{user.createdAt}</Td>}
+                    </Tr>
+                  ))}
                 </Tbody>
               </Table>
 
