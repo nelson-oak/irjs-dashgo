dc4322df0bc6c8f783b0964c0e7057b5ce340b52
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-05-26 19:45:53 -0300

    Aula 'Configurando React Query' (C4-M2-SM1)

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,22 +1,28 @@
-import { Checkbox, Box, Button, Flex, Heading, Icon, Table, Th, Thead, Tr, Tbody, Td, Text, useBreakpointValue } from "@chakra-ui/react";
+import { Checkbox, Box, Button, Flex, Heading, Icon, Table, Th, Thead, Tr, Tbody, Td, Text, useBreakpointValue, Spinner } from "@chakra-ui/react";
 import Link from "next/link";
 import { useEffect } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { useQuery } from 'react-query'
+
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users');
+    const data = response.json()
+    
+    return data
+  })
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
   })
 
   useEffect(() => {
-    fetch('http://localhost:3000/api/users')
-      .then(response => response.json())
-      .then(data => console.log(data))
-  })
+    })
 
   return (
     <Box>
@@ -62,107 +68,124 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table
-            colorScheme="whiteAlpha"
-          >
-            <Thead>
-              <Tr>
-                <Th
-                  px={['4', '4', '6']}
-                  color="gray.300"
-                  w="8"
-                >
-                  <Checkbox
-                    colorScheme="pink"
-                  />
-                </Th>
-                <Th>Usuário</Th>
-                { isWideVersion && <Th>Data de cadastro</Th>}
-              </Tr>
-            </Thead>
-
-            <Tbody>
-              <Tr>
-                <Td
-                  px={['4', '4',]}
-                >
-                  <Checkbox
-                    colorScheme="pink"
-                  />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text
-                      fontWeight="bold"
-                    >
-                      Nelson Carvalho
-                    </Text>
-                    <Text
-                      fontSize="small"
+          
+          {isLoading ? (
+            <Flex
+              justify="center"
+            >
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex
+              justify="center"
+            >
+              <Text>Falha ao obter dados dos usuários.</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table
+                colorScheme="whiteAlpha"
+              >
+                <Thead>
+                  <Tr>
+                    <Th
+                      px={['4', '4', '6']}
                       color="gray.300"
+                      w="8"
                     >
-                      nelson.oak.13@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>13 de Março, 2021</Td>}
-              </Tr>
+                      <Checkbox
+                        colorScheme="pink"
+                      />
+                    </Th>
+                    <Th>Usuário</Th>
+                    { isWideVersion && <Th>Data de cadastro</Th>}
+                  </Tr>
+                </Thead>
 
-              <Tr>
-                <Td
-                  px={['4', '4',]}
-                >
-                  <Checkbox
-                    colorScheme="pink"
-                  />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text
-                      fontWeight="bold"
+                <Tbody>
+                  <Tr>
+                    <Td
+                      px={['4', '4',]}
                     >
-                      Nelson Carvalho
-                    </Text>
-                    <Text
-                      fontSize="small"
-                      color="gray.300"
-                    >
-                      nelson.oak.13@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>13 de Março, 2021</Td>}
-              </Tr>
+                      <Checkbox
+                        colorScheme="pink"
+                      />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text
+                          fontWeight="bold"
+                        >
+                          Nelson Carvalho
+                        </Text>
+                        <Text
+                          fontSize="small"
+                          color="gray.300"
+                        >
+                          nelson.oak.13@gmail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>13 de Março, 2021</Td>}
+                  </Tr>
 
-              <Tr>
-                <Td
-                  px={['4', '4',]}
-                >
-                  <Checkbox
-                    colorScheme="pink"
-                  />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text
-                      fontWeight="bold"
+                  <Tr>
+                    <Td
+                      px={['4', '4',]}
                     >
-                      Nelson Carvalho
-                    </Text>
-                    <Text
-                      fontSize="small"
-                      color="gray.300"
-                    >
-                      nelson.oak.13@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>13 de Março, 2021</Td>}
-              </Tr>
-            </Tbody>
-          </Table>
+                      <Checkbox
+                        colorScheme="pink"
+                      />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text
+                          fontWeight="bold"
+                        >
+                          Nelson Carvalho
+                        </Text>
+                        <Text
+                          fontSize="small"
+                          color="gray.300"
+                        >
+                          nelson.oak.13@gmail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>13 de Março, 2021</Td>}
+                  </Tr>
 
-          <Pagination />
+                  <Tr>
+                    <Td
+                      px={['4', '4',]}
+                    >
+                      <Checkbox
+                        colorScheme="pink"
+                      />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text
+                          fontWeight="bold"
+                        >
+                          Nelson Carvalho
+                        </Text>
+                        <Text
+                          fontSize="small"
+                          color="gray.300"
+                        >
+                          nelson.oak.13@gmail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>13 de Março, 2021</Td>}
+                  </Tr>
+                </Tbody>
+              </Table>
+
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
