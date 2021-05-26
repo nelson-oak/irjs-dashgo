6fac8afabc63b258a67976b5d17d887742548204
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-05-26 20:58:19 -0300

    Aula 'Criando hook de listagem' (C4-M2-SM2)

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -2,34 +2,14 @@ import { Checkbox, Box, Button, Flex, Heading, Icon, Table, Th, Thead, Tr, Tbody
 import Link from "next/link";
 import { useEffect } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
-import { useQuery } from 'react-query'
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
-import { api } from "../../services/api";
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error } = useQuery('users', async () => {
-    const { data } = await api.get('/users');
-
-    const users = data.users.map(user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric'
-        })
-      }
-    })
-
-    return users
-  }, {
-    staleTime: 1000 * 5 // 5 seconds
-  })
+  const { data, isLoading, isFetching, error } = useUsers()
 
   const isWideVersion = useBreakpointValue({
     base: false,
