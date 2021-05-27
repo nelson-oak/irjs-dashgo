411800b532dc4dba84f8519341c71d28c59e9729
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-05-26 22:50:54 -0300

    Aula 'Utilizando mutations' (C4-M2-SM2)

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -8,6 +8,10 @@ import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { useMutation } from "react-query";
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
+import { useRouter } from "next/dist/client/router";
 
 type CreateUserFormData = {
   name: string
@@ -26,6 +30,25 @@ const createUserFormSchema = yup.object().shape({
 })
 
 export default function CreateUser() {
+  const router = useRouter()
+
+  const createUser = useMutation(async (user: CreateUserFormData) => {
+    const response = await api.post('users', {
+      user: {
+        ...user,
+        created_at: new Date()
+      }
+    })
+
+    return response.data.user
+  }, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('users')
+
+      router.push('/users')
+    }
+  })
+
   const { register, handleSubmit, formState } = useForm({
     resolver: yupResolver(createUserFormSchema)
   })
@@ -33,8 +56,7 @@ export default function CreateUser() {
   const { errors } = formState
 
   const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
-    await new Promise(resolve => setTimeout(resolve, 2000))
-    console.log(values)
+    await createUser.mutateAsync(values)
   }
 
   return (
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -19,7 +19,7 @@ export default function UserList() {
     lg: true
   })
 
-  async function handlePrefetchUser(userId: number) {
+  async function handlePrefetchUser(userId: string) {
     await queryClient.prefetchQuery(['user', userId], async () => {
       const response = await api.get(`users/${userId}`)
 
@@ -126,7 +126,7 @@ export default function UserList() {
                         <Box>
                           <ChakraLink
                             color="purple.400"
-                            onMouseEnter={() => handlePrefetchUser(Number(user.id))}
+                            onMouseEnter={() => handlePrefetchUser(user.id)}
                           >
                             <Text
                               fontWeight="bold"
